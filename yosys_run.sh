285d0b9bd63624a87a3d98d1f7f0be44ccefc48d
G.RAVI KIRAN REDDY <110079770+Gogireddyravikiran@users.noreply.github.com>
2022-08-06 11:30:05 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -6,9 +6,9 @@ read_verilog iiitb_sd_fsm.v
 synth -top iiitb_sd_fsm
 
 # mapping to mycells.lib
-dfflibmap -liberty /home/gogireddyravikiranreddy1/Desktop/iiitb_sd_fsm/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-abc -liberty /home/gogireddyravikiranreddy1/Desktop/iiitb_sd_fsm/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-clean
+dfflibmap -liberty /home/ravi/iiitb_sd_fsm/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/ravi/iiitb_sd_fsm/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
 flatten
 # write synthesized design
 write_verilog -noattr iiitb_sd_fsm_synth.v
+
